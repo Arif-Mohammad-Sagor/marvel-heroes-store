21b37b8ee8613f46ca023256fa6ce7d10da1759a
MdTalha707 <mdsagor0321@gmail.com>
2023-05-18 17:53:52 +0600

    category section addeed

--- src/pages/Home/ShopCategory.jsx
+++ src/pages/Home/ShopCategory.jsx
@@ -1,36 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
 
-
-
 const ShopCategory = () => {
+    const [tabIndex, setTabIndex] = useState(0);
+    const [toys, setToys] = useState([]);
+
+    console.log(tabIndex);
+
+    useEffect(() => {
+      if (tabIndex === 0) {
+        fetch("http://localhost:5000/allToys/Avengers")
+          .then((res) => res.json())
+          .then((data) => setToys(data));
+      } else if (tabIndex === 1) {
+        fetch("http://localhost:5000/allToys/Guardians of the Galaxy")
+          .then((res) => res.json())
+          .then((data) => setToys(data));
+      } else if (tabIndex === 2) {
+        fetch("http://localhost:5000/allToys/Civil War")
+          .then((res) => res.json())
+          .then((data) => setToys(data));
+      }
+    }, [tabIndex]);
+    console.log(toys);
+
   return (
     <div className="my-8">
       <p className="text-5xl font-serif mb-6 text-center py-8">
         Shop By Category
       </p>
 
-      <Tabs>
+          <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index) }>
         <TabList>
           <Tab>Civil War</Tab>
-          <Tab>Guardians of The Galaxy</Tab>
+          <Tab>Avengers</Tab>
           <Tab>Rangkok</Tab>
         </TabList>
 
         <TabPanel>
+                  <div className="grid md:grid-cols-2">
+                      {toys && toys.map(toy => <div key={toy._id}>
+                          <p>{toy?.Name}</p>
+                      </div>)}
+          </div>
+        </TabPanel>
 
-                  <div className='grid md:grid-cols-2'>
-
-                  </div>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 3</h2>
-        </TabPanel>
       </Tabs>
     </div>
   );
